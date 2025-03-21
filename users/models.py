a587812e0a4225d952e30bd6cb4e94941f8a5b76
A-vi33 <vigneshmakala@gmail.com>
2025-03-21 17:06:51 +0530

    Updated

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.db.models import CharField
+
 from cars.models import Car
 
 # In users/models.py
@@ -29,9 +31,9 @@ class Order(models.Model):
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     street_address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
+    city = models.CharField(max_length=100,default=CharField)
     state = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20)
+    postal_code = models.IntegerField(max_length=20)
     phone = models.CharField(max_length=15)
 
     def __str__(self):
